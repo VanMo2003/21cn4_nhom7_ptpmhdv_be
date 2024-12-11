b1c5cad5dc109e816ba7894c0d932fce6675b7ba
VanMo3923 <mjnhhuyen0@gmail.com>
2024-12-11 11:11:41 +0700

    create user

---
+++ src/main/java/com/example/booking_app_be/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.example.booking_app_be.repository;
+
+import com.example.booking_app_be.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, String> {
+    boolean existsByUsername(String username);
+
+    Optional<User> findByUsername(String username);
+}
